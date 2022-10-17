d80d0618f24eb60b01d605d258ba750ff58b29f7
Oussama Khalifeh <oukhali@hotmail.com>
2022-10-17 16:29:50 -0400

    Added redux to project

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,19 +4,23 @@ import './index.css';
 import { App } from './modules/main';
 import * as serviceWorker from './serviceWorker';
 import Session from './modules/main/components/Session';
+import store from "./store";
+import { Provider } from "react-redux";
 
 const sessionRef = React.createRef();
-const session = <Session ref={sessionRef} showSession={false} pollAPI={true}/> // pollAPI cannot accurately tell us if the user's session has expired
+const session = <Session ref={sessionRef} showSession={false} pollAPI={true} /> // pollAPI cannot accurately tell us if the user's session has expired
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App sessionRef={sessionRef}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App sessionRef={sessionRef} />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 ReactDOM.render(
-  session,
-  document.getElementById('session')
+	session,
+	document.getElementById('session')
 );
 
 // If you want your app to work offline and load faster, you can change
