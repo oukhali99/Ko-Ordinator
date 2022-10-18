a4394ef02e68d3e87d9559c39eb7a55c8f9d17c9
Oussama Khalifeh <oukhali@hotmail.com>
2022-10-17 21:35:33 -0400

    Fixed main.reducer overwrites

--- Frontend/src/modules/main/redux/main.reducer.js
+++ Frontend/src/modules/main/redux/main.reducer.js
@@ -20,10 +20,18 @@ const initialState = {
 
 export default createReducer(initialState, {
     [mainActions.setAppStateAction]: (state, action) => {
-        state.appState = action.payload;
+        state.appState.loggedIn = action.payload.loggedIn || state.appState.loggedIn;
     },
     [mainActions.setSessionStateAction]: (state, action) => {
-        state.sessionState = action.payload;
+        state.sessionState.pollAPI = action.payload.loggedIn || state.sessionState.pollAPI;
+        state.sessionState.username = action.payload.username || state.sessionState.username;
+        state.sessionState.sessionId = action.payload.sessionId || state.sessionState.sessionId;
+        state.sessionState.userId = action.payload.userId || state.sessionState.userId;
+        state.sessionState.availabilities = action.payload.availabilities || state.sessionState.availabilities;
+        state.sessionState.friends = action.payload.friends || state.sessionState.friends;
+        state.sessionState.friendRequests = action.payload.friendRequests || state.sessionState.friendRequests;
+        state.sessionState.groups = action.payload.groups || state.sessionState.groups;
+        state.sessionState.sessionTimestamp = action.payload.sessionTimestamp || state.sessionState.sessionTimestamp;
     },
     [mainActions.clearSessionStateAction]: (state, action) => {
         state = initialState;
